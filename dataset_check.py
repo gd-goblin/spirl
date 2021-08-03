c6810512f358cf9df150d981659048ecfca50ca9
twkim <twkim0812@gmail.com>
2021-08-03 21:27:28 +0900

    - dataset with image viewer

--- dataset_check.py
+++ dataset_check.py
@@ -3,9 +3,20 @@ import os
 import numpy as np
 from spirl.utils.general_utils import AttrDict
 
-data_path = './data/office_TA'
+import cv2
+
+# data_path = './data/office_TA'
+data_path = './data/block_stacking'
 phase_list = os.listdir(data_path)
+try:
+    idx = phase_list.index('.DS_Store')
+    if idx is not None:
+        del phase_list[idx]
+except ValueError:
+    pass
+
 phase_list.sort(key=lambda f: int(''.join(filter(str.isdigit, f))))
+print(phase_list)
 phase_num = 0
 
 filename = "rollout_" + str(0) + '.h5'
@@ -13,20 +24,39 @@ path = os.path.join(data_path, phase_list[phase_num], filename)
 print('path: ', path)
 
 index = 0
-with h5py.File(path, "r") as f:
-    # # List all groups
-    # print("Keys: %s" % f.keys())
-    # a_group_key = list(f.keys())[0]
-    #
-    # # Get the data
-    # data = list(f[a_group_key])
-    # print("data: ", data)
+frame = 0
+while True:
+    with h5py.File(path, "r") as f:
+        # List all groups
+        print("Keys: %s" % f.keys())
+        a_group_key = list(f.keys())[0]
 
-    data = AttrDict()
+        # Get the data
+        data = list(f[a_group_key])
+        print("data: ", data)
 
-    key = 'traj{}'.format(index)
-    # Fetch data into a dict
-    for name in f[key].keys():
-        if name in ['states', 'actions', 'pad_mask']:
-            data[name] = f[key + '/' + name][()].astype(np.float32)
-            print("{}: shape: {}, data: {}".format(name, data[name].shape, data[name]))
+        data = AttrDict()
+
+        key = 'traj{}'.format(index)
+        # Fetch data into a dict
+        for name in f[key].keys():
+            if name in ['states', 'actions', 'pad_mask']:
+                data[name] = f[key + '/' + name][()].astype(np.float32)
+                # print("{}: shape: {}, data: {}".format(name, data[name].shape, data[name]))
+                print("{}: shape: {}".format(name, data[name].shape))
+
+        if key + '/images' in f:
+            # data.images = f[key + '/images'][()]
+            data.images = f[key + '/images'][()]
+            print("images: shape: {}, type: {}".format(data.images.shape, type(data.images)))
+            img = data.images[frame]
+
+            cv2.imshow("image", img)
+            k = cv2.waitKey(0) & 0xFF
+            if k == 27:
+                cv2.destroyAllWindows()
+                break
+            elif k == ord('d'):
+                frame = min(frame + 1, data[name].shape[0] - 1)
+            elif k == ord('a'):
+                frame = max(frame - 1, 0)
